f9b70bdcf4ac41c7482c7b58a2096e815619011e
Lucas Larrubia <Lucas@Marcias-iMac.local>
2019-05-03 21:21:04 -0400

    added preventDefault()

--- js/index.js
+++ js/index.js
@@ -4,30 +4,30 @@ const h2Header = document.querySelectorAll('h2');
 for (let i = 0; i < h2Header.length; i++) {
     h2Header[i].addEventListener('mouseover', () => {
     h2Header[i].style.color = 'gray'; 
-    })
+    });
     h2Header[i].addEventListener('mouseout', () => {
         h2Header[i].style.color = '';
-    })
+    });
 }
 
 const image = document.querySelectorAll('img');
 for (let i = 0; i < image.length; i++) {
     image[i].addEventListener('click', () => {
     image[i].style.opacity = '0.5';
-    })
+    });
     image[i].addEventListener('mouseover', () => {
         image[i].style.border = '3px solid pink';
-    })
+    });
     image[i].addEventListener('mouseout', () => {
         image[i].style.border = '';
-    })
+    });
 }
 
 const button = document.querySelectorAll('.btn');
 for (let i = 0; i < button.length; i++) {
     button[i].addEventListener('drag', () => {
         image[i].style.opacity = '0.5';
-    })
+    });
 }
 
 const h1Header = document.querySelector('h1');
@@ -45,6 +45,12 @@ for (let i = 0; i < navItems.length; i++) {
     })
     navItems[i].addEventListener('mouseover', () => {
         navItems[i].classList.toggle('large');
-})
-
+    });
 }
+
+// prevent default 
+
+const stopNav = document.querySelector('.nav');
+stopNav.addEventListener('click', e => {
+    e.preventDefault();
+});
